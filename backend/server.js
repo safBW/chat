b03817cff237a193627209cf711d8cdd87dc2f1b
saf <lcscode@outlook.com>
2024-02-29 15:30:28 +0800

    completed all - socket

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongodb.js";
+import { server,app } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
@@ -19,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
